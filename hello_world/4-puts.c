54ddb0e967bdac7b20e917bd567b50e4700da693
Abimael <icemask2612@gmail.com>
2024-02-19 13:30:07 -0400

    fini

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - start the program
  *
  * Return: 0
  *
  * */
+
 int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
