7d81b61ce68c4e69f170c3abd5ee1f5cdd6acf64
Abimael <icemask2612@gmail.com>
2024-02-20 20:38:27 -0400

    numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,13 +10,13 @@ int main(void)
 {
 int x;
 
-for (x = 48; x < 58; x++)
+for (x = 0; x < 10; x++)
 {
-	putchar(x);
-	if (x < 57)
+	putchar(x + '0');
+	if (x < 9)
 	{
-	putchar(44);
-	putchar(32);
+	putchar(',');
+	putchar(' ');
 	}
 }
 putchar('\n');
