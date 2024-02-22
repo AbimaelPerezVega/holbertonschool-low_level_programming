0d977ef290dacecca7e843eaabb294a3d1f131cb
Abimael <icemask2612@gmail.com>
2024-02-22 14:37:21 -0400

    number 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - more alpha
+ *
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+if (i != 9)
+	_putchar('\n');
+}
+}
