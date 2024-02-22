90ea16469bd32316280f4ecf12159912cf56553c
Abimael <icemask2612@gmail.com>
2024-02-22 14:13:26 -0400

    alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - make the aplha
+ *
+ * Return: void
+*/
+void print_alphabet(void)
+{
+
+	char c;
+	
+	for(c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	_putchar('\n');
+}
