240d7b65bb24f1cae83fc214bb1f7684db00cae5
Abimael <icemask2612@gmail.com>
2024-02-23 10:48:34 -0400

    a

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,11 +6,10 @@
 */
 int _isalpha(int c)
 {
-
-	for ((c >= 65 && c <= 90) || (c = 97 && c <= 122))
-	{
-	return (1);
-	}
-	return (0);
+for ((c >= 65 && c <= 90) || (c = 97 && c <= 122))
+{
+return (1);
+}
+return (0);
 }
 
