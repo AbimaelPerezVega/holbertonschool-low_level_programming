e8ab1c44119ae114c65e92e63185f67b117c1cd6
Abimael <icemask2612@gmail.com>
2024-02-23 14:32:22 -0400

    idk

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - compute a value of an integer
+ *@n: integer to compute
+ *Return: Always 0
+*/
+int _abs(int n);
+{
+if (n < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (n);
+}
