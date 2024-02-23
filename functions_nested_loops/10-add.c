66510fe68bb93a6839429e25480c63e3bdb40afc
Abimael <icemask2612@gmail.com>
2024-02-23 17:44:59 -0400

    wtf

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @n1: first number to be computed
+ * @n2: second
+ * Return: Integer value
+*/
+int add(int n1, int n2)
+{
+	return  (n1 + n2);
+}
