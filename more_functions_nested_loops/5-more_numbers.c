788f246a9adf81d4291c13c4ebe268f81b03f5e5
Abimael <icemask2612@gmail.com>
2024-02-26 12:57:42 -0400

    maybe

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers - adding 0 to 14 ten times
+ *
+ *Return: void
+ *
+*/
+void more_numbers(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+char c;
+for (c = '0'; c <= "14"; c++)
+{
+_putchar(c);
+}
+if (n != 10)
+	_putchar('\n');
+}
+}
