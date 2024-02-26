0d42e3f5ab8b91179a53c6cc278ea7216a5eb995
Abimael <icemask2612@gmail.com>
2024-02-26 13:19:31 -0400

    heyy

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,13 @@ void more_numbers(void)
 int n, c;
 for (n = 0; n < 10; n++)
 {
-for (c = '0'; c <= "14"; c++)
+for (c = '0'; c <= "9"; c++)
 {
+_putchar(c);
+}
+for (c = '0'; c <= '4'; c++)
+{
+_putchar('1');
 _putchar(c);
 }
 _putchar('\n');
