7d51cfbb98d7499adb5aff6afe7ee523d877b210
Abimael <icemask2612@gmail.com>
2024-02-26 10:09:09 -0400

    maybe first try

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - uppercase
+ *@c: the character
+ *Return: 1 for C or 0 for anyhting else
+*/
+int _isupper(int c);
+{
+if (c == C)
+{
+return (1);
+}
+return (0);
+}
