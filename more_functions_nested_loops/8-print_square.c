b74d4309ec7e3d31fec47a6c7af5c08984df547b
Abimael <icemask2612@gmail.com>
2024-02-27 09:57:01 -0400

    not my day

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - making a square with hashtags
+ *@size: creating a hashtag square
+ *Return: empty
+ *
+*/
+void print_square(int size)
+{
+int q, t;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (q = 0; q < size; q++)
+{
+for (t = 0; t < q; t++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
