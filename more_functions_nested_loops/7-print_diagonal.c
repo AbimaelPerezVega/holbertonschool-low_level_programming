67f1d39b4ceac54ea9bedaf77df932e82b969930
Abimael <icemask2612@gmail.com>
2024-02-27 09:09:54 -0400

    help

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,11 +2,11 @@
 /**
  * print_diagonal - making the diagonal line
  *@n: number of times creating diagonal line
- *Return: void
+ *Return: empty
 */
 void print_diagonal(int n)
 {
-int d, space;
+int d, s;
 if (n <= 0)
 {
 _putchar('\n');
@@ -15,12 +15,12 @@ else
 {
 for (d = 0; d < n; d++)
 {
-	for (space = 0; space < d; space++)
+for (s = 0; s < d; s++)
 {
-	_putchar(' ');
+_putchar(32);
 }
-_putchar('\\');
-}
+_putchar(92);
 _putchar('\n');
+}
 }
 }
