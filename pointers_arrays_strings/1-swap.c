9c7cf9ec3b9a4cffaee6e70f278e7b791f29fa83
Abimael <icemask2612@gmail.com>
2024-02-28 10:31:44 -0400

    im overcomp

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * swap_int - swapping the variables
  * @a: swapping variable
@@ -13,18 +12,4 @@ int temp;
 temp = *a;
 *a = *b;
 *b = temp;
-}
-
-/**
- * main - testing variables
- * Return: 0
-*/
-int main(void)
-{
-int m = 25;
-int n = 100;
-_putchar("m is %d, n is %d\n", m, n);
-swap_int(&m, &n);
-_putchar("m is %d, n is %d\n", m, n);
-return (0);
 }
