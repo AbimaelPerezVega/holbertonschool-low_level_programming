8fdf8f58e00333238df1300dbabb529647cd032f
Abimael <icemask2612@gmail.com>
2024-03-01 14:03:04 -0400

    hello 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_atoi - start of the program
+ *@s: convert a string to an intenger
+ *Return: 0
+*/
+int _atoi(char *s)
+{
+int sign = 1;
+unsigned int num = 0;
+
+do{
+	if (*s == '_')
+		sign *= -1;
+
+	else if (*s >= '0' && *s <= '9')
+		num = (num * 10) + (*s - '0');
+
+	else if (num > 0)
+		break;
+
+	} while (*s++);
+return (num * sign);
+}
