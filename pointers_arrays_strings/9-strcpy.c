25615e9759beb9d2db5f43e74fc31c6adff1bbd4
Abimael <icemask2612@gmail.com>
2024-03-01 13:09:18 -0400

    uhhh

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcpy - copying the string pointed by src
+ *@dest: destination to copy
+ *@src: source to copy
+ *Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int t;
+for (t = 0; src[t] != '\0';)
+{
+t++;
+}
+{
+dest[t] = src[t];
+}
+dest[t++] = '\0';
+return (dest);
+}
