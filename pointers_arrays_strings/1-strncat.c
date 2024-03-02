def85dc7bd483cbde00e19a1d38b2898aa4acc0e
Abimael <icemask2612@gmail.com>
2024-03-02 19:11:47 -0400

    string

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -5,13 +5,13 @@
  *@src: the string to be appended to dest
  *@n: the number of bytes
  *Return: a pointer to the resulting string dest
- *
- */
+*/
 char *_strncat(char *dest, char *src, int n)
+
 {
 int index = 0, dest_len = 0;
 
-while (dest(index++))
+while (dest[index++])
 	dest_len++;
 for (index = 0; src[index] && index < n; index++)
 	dest[dest_len++] = src[index];
