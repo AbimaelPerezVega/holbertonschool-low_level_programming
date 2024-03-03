c93560854d614f89f045ebfed641ebe30a9016c7
Abimael <icemask2612@gmail.com>
2024-03-02 20:30:49 -0400

    fixing bett

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *_strcmp - comparing the pointers 
+ *_strcmp - comparing the pointers
  *@s1: A pointer to the first string being compared
  *@s2: A pointer to the second string to be compared
- *Return: If str1 < str2, the negative difference of the first unmatched characters
+ *Return: If str1 < str2, the negative difference unmatched characters
  *If str1 == str2, 0.
  *If str1 > str2, the positive deifference of the first characters
 */
