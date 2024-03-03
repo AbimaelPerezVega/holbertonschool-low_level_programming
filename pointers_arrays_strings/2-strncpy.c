fdcf24fca8c14a8a839a0ab36eda10678c52434a
Abimael <icemask2612@gmail.com>
2024-03-02 20:28:35 -0400

    another one

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int index = 0, slen = 0;
 
 	while (src[index++])
-		src_len++;
+		slen++;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
