31b649db30eacc15f24f0996152a350db479443b
Abimael <icemask2612@gmail.com>
2024-03-02 21:43:55 -0400

    uhg

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 /**
  * string_toupper - making the lowercases upper
- *@s: the string to upper
+ *@c: the string to upper
  *
  *Return: 0
 */
 char *string_toupper(char *)
 {
-char * s = str;
+int c;
 
-while (*s)
-{
-*s = (*s > 'a' && *s <= 'z') ? *s-32 : *s;
-s++;
-}
+if ((c >= 'A' && c <= 'Z'))
+
 return (0);
 }
