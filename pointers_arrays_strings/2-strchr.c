6a7712de0b7073a9f4b6131ab249e4f6caadf08c
Abimael <icemask2612@gmail.com>
2024-03-05 10:13:42 -0400

    maybe

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character
+ *@s: searching the string
+ *@c: the location
+ *Return: null
+ *
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+	return (s);
+s++;
+}
+return (NULL);
+}
