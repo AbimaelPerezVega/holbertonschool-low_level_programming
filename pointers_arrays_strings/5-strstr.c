5ecaf7f1623445bc4ea59abb5df9f80841db96bc
Abimael <icemask2612@gmail.com>
2024-03-05 14:37:36 -0400

    a

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ *@haystack: string to search
+ *@needle: substring to locate
+ *Return: null
+ *
+*/
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack != '\0')
+{
+char *temp_haystack = haystack;
+char *temp_needle = needle;
+
+while (*temp_needle != '\0' && temp_haystack == *temp_needle)
+{
+temp_haystack++;
+temp_needle++;
+}
+if (*temp_needle == '\0')
+{
+return haystack;
+}
+haystack++;
+}
+return (NULL);
+}
