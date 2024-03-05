9e95a31f73b377e40a45ce2502e979cc3c327685
Abimael <icemask2612@gmail.com>
2024-03-05 09:26:53 -0400

    test

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_memcpy - copy memory area
+ *@dest: the destination
+ *@src: the source
+ *@n: bytes
+ *Return: always 0
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i--)
+	dest[i] = src;
+return(dest);
+}
