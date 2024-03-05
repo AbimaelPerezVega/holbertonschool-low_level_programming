1f9b848c572c0cfd681e3ca010cdc7d673f60b4c
Abimael <icemask2612@gmail.com>
2024-03-05 13:51:03 -0400

    maybe

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix
+ *@s: the string
+ *@accept: the characters
+ *Return: number of bytes
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+int f;
+
+while (*s != '\0') {
+	f = 0;
+
+	while (*accept != '\0') {
+		if (*s == *accept) {
+			found = 1;
+			break;
+		}
+		accept++
+	}
+	if (!found) {
+		break;
+	}
+	count++;
+	s++;
+	accept = original_accept;
+}
+return (count);
+}
