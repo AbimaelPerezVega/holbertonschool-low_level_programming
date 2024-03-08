632034fa496fc2ecb26a6537cdf6b1356a735392
Abimael <icemask2612@gmail.com>
2024-03-08 13:56:45 -0400

    fixing

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,7 +11,7 @@
 */
 int main(int argc, char *argv[])
 {
-int i, sum = 0, j = 0;
+int i, j, sum = 0;
 
 if (argc == 1)
 {
@@ -23,10 +23,10 @@ for (i = 1; i < argc; i++)
 for (j = 0; argv[i][j] != '\0'; j++)
 {
 if (!isdigit(argv[i][j]))
-		{
-		printf("Error\n");
-		return (1);
-		}
+{
+printf("Error\n");
+return (1);
+}
 }
 sum += atoi(argv[i]);
 }
