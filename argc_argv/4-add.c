68dda8f32b6c1a97d61b387441e9ffc8262049a6
Abimael <icemask2612@gmail.com>
2024-03-08 13:49:29 -0400

    positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry of the program
+ *@argc: number of command-line
+ *@argv: array of strings
+ *
+ *Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+int i, sum = 0, j = 0;
+
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+
+return (0);
+}
