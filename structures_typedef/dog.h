a01765cfdd6976a86824e7b4b46abcd0adb11f41
Abimael <icemask2612@gmail.com>
2024-03-15 11:47:14 -0400

    u make me look bad

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,5 +15,5 @@ char *owner;
 };
 typedef struct dog dog_t;
 
-void init_dog(dog_t *d, char *name, float age, char *owner); 
+void init_dog(dog_t *d, char *name, float age, char *owner);
 #endif /* DOG_H */
