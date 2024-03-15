66791c4ece0a44ebfafd61a2c62c767662cf3c64
Abimael <icemask2612@gmail.com>
2024-03-15 11:46:16 -0400

    stupid dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,10 +12,10 @@
 void init_dog(dog_t *d, char *name, float age, char *owner)
 {
 if (d != NULL)
-  {
-     d->name = name;
-     d->age = age;
-     d->owner = owner;
-  }
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
 
