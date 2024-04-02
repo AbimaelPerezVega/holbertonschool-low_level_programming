aea192625184ce443bc070f32c1fbec3b7e72afe
Abimael <icemask2612@gmail.com>
2024-04-02 10:54:20 -0400

    list len

--- singly_linked_lists/1-main.c
+++ singly_linked_lists/1-main.c
@@ -14,6 +14,7 @@ list_t *head;
 list_t *new;
 list_t hello = {"World", 5, NULL};
 size_t n;
+
 head = &hello;
 new = malloc(sizeof(list_t));
 if (new == NULL)
@@ -25,13 +26,9 @@ new->str = strdup("Hello");
 new->len = 5;
 new->next = head;
 head = new;
-n = print_list(head);
+n = list_len(head);
 printf("-> %lu elements\n", n);
-printf("\n");
 free(new->str);
-new->str = NULL;
-n = print_list(head);
-printf("-> %lu elements\n", n);
 free(new);
 return (0);
 }
