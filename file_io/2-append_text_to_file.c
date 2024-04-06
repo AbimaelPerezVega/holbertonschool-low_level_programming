5cc35bf57272638b5487c1cdf8938d2e861a004e
Abimael <icemask2612@gmail.com>
2024-04-06 16:25:56 -0400

    fixes

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int file;
-ssize_t length = 0, inlen = 0;
+ssize_t length, inlen;
 char *ptr;
 
 if (filename == NULL)
@@ -24,12 +24,11 @@ file = open(filename, O_WRONLY | O_APPEND);
 if (file == -1)
 	return (-1);
 
-if (text_content != NULL)
-{
+
 for (inlen = 0; ptr = text_content; *ptr; ptr++)
 	inlen++;
 length = write(file, text_content, inlen);
-}
+
 if (close(file) == -1 || inlen != length)
 	return (-1);
 return (1);
